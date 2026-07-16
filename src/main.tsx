c7fdf45460a3bbed48963cb92b8e00e60750d8a4
tyagianiket <anikettyagi.anr@gmail.com>
2026-07-17 02:10:43 +0530

    cursor changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from './ThemeContext'
 import App from './App'
-import UsPage from './app/UsPage'
+import UsPage from './app/UsPage' // Ensure this path is correct based on your file tree
 import './index.css'
 
+// This is the gatekeeper
 const currentPath = window.location.pathname;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
